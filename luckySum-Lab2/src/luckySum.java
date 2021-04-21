bc2f5dc48daa1e83ef0edaa169d43d892c4d9a2f
AMELIE MUELLER <s0577540@htw-berlin.de>
2021-04-21 17:11:17 +0200

    modified commands, luckySum java project

---
+++ luckySum-Lab2/src/luckySum.java
@@ -0,0 +1,21 @@
+public class luckySum{
+
+	public static void main(String[] args) {
+		luckySum sum = new luckySum();
+		int a = 1;
+		int b = 13;
+		int c = 5;
+		System.out.println(sum.luckySumAlgebra(a, b, c));
+	}
+	
+	public int luckySumAlgebra(int a, int b, int c) {
+		int sum = 0;
+		
+		//return sum of a,b,c
+		
+		//but if one value is 13, only the values before get added to sum
+		// example: a=3, b=13, c=2 ==> sum = a; a=1, b=4, c=13 ==> sum = a+b
+		
+		return sum;
+	}
+}
